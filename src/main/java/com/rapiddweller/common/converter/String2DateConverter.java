ce0275174e8044c22ac8b8b632a58cf074b9e5a1
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-10-02 19:54:10 +0200

    Improved code quality

--- src/main/java/com/rapiddweller/common/converter/String2DateConverter.java
+++ src/main/java/com/rapiddweller/common/converter/String2DateConverter.java
@@ -28,51 +28,27 @@ import java.util.Locale;
 /**
  * Converts Strings of standard date(time) format (yyyy-MM-dd[Thh:mm[:ss[.SSS]]]) to dates.
  * Created: 07.09.2007 09:07:12
- *
  * @param <E> the {@link Date} type to convert to
  * @author Volker Bergmann
  */
 public class String2DateConverter<E extends Date> extends ThreadSafeConverter<String, E> implements Patterns {
 
-  // TODO support time zones (like 'Z', '+01:00' or '-01:30')
-
   private final String pattern;
   private final Locale locale;
 
-  /**
-   * Instantiates a new String 2 date converter.
-   */
   public String2DateConverter() {
     this(null);
   }
 
-  /**
-   * Instantiates a new String 2 date converter.
-   *
-   * @param pattern the pattern
-   */
   public String2DateConverter(String pattern) {
     this(pattern, Locale.getDefault());
   }
 
-  /**
-   * Instantiates a new String 2 date converter.
-   *
-   * @param pattern the pattern
-   * @param locale  the locale
-   */
   @SuppressWarnings("unchecked")
   public String2DateConverter(String pattern, Locale locale) {
     this(pattern, locale, (Class<E>) java.util.Date.class);
   }
 
-  /**
-   * Instantiates a new String 2 date converter.
-   *
-   * @param pattern    the pattern
-   * @param locale     the locale
-   * @param targetType the target type
-   */
   public String2DateConverter(String pattern, Locale locale, Class<E> targetType) {
     super(String.class, targetType);
     this.pattern = pattern;
@@ -85,16 +61,6 @@ public class String2DateConverter<E extends Date> extends ThreadSafeConverter<St
     return (E) convert(sourceValue, pattern, locale, targetType);
   }
 
-  /**
-   * Convert date.
-   *
-   * @param <T>         the type parameter
-   * @param sourceValue the source value
-   * @param pattern     the pattern
-   * @param locale      the locale
-   * @param targetType  the target type
-   * @return the date
-   */
   public static <T extends Date> Date convert(String sourceValue, String pattern, Locale locale, Class<T> targetType) {
     sourceValue = StringUtil.trimmedEmptyToNull(sourceValue);
     if (sourceValue == null) {
@@ -105,7 +71,7 @@ public class String2DateConverter<E extends Date> extends ThreadSafeConverter<St
       sourceValue = sourceValue.replace(' ', 'T');
       if (pattern != null) {
         format = new SimpleDateFormat(pattern, locale);
-      } else if (sourceValue.indexOf('T') > 0) {
+      } else if (sourceValue.indexOf('T') >= 0) {
         switch (sourceValue.length()) {
           case 16:
             format = new SimpleDateFormat(DEFAULT_DATETIME_MINUTES_PATTERN);
