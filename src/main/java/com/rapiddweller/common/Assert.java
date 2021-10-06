ee5e86e0317db4b82dafd9c4bca5ac68281886e7
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-10-06 15:53:07 +0200

    Added equals(Object, Object) method without message parameter

--- src/main/java/com/rapiddweller/common/Assert.java
+++ src/main/java/com/rapiddweller/common/Assert.java
@@ -23,7 +23,6 @@ import java.util.Set;
 /**
  * An assertion utility.
  * Created at 25.04.2008 17:59:43
- *
  * @author Volker Bergmann
  * @since 0.4.2
  */
@@ -34,12 +33,6 @@ public class Assert {
   private Assert() {
   }
 
-  /**
-   * End.
-   *
-   * @param text the text
-   * @param end  the end
-   */
   public void end(String text, String end) {
     if (text == null) {
       if (end != null) {
@@ -50,12 +43,6 @@ public class Assert {
     }
   }
 
-  /**
-   * End ignore case.
-   *
-   * @param text the text
-   * @param end  the end
-   */
   public static void endIgnoreCase(String text, String end) {
     if (text == null) {
       if (end != null) {
@@ -67,14 +54,6 @@ public class Assert {
     }
   }
 
-  /**
-   * Not null t.
-   *
-   * @param <T>        the type parameter
-   * @param object     the object
-   * @param objectRole the object role
-   * @return the t
-   */
   public static <T> T notNull(T object, String objectRole) {
     if (object == null) {
       throw new IllegalArgumentException(objectRole + " must not be null");
@@ -82,13 +61,6 @@ public class Assert {
     return object;
   }
 
-  /**
-   * Not empty string.
-   *
-   * @param text    the text
-   * @param message the message
-   * @return the string
-   */
   public static String notEmpty(String text, String message) {
     if (text == null || text.length() == 0) {
       throw new IllegalArgumentException(message);
@@ -96,55 +68,39 @@ public class Assert {
     return text;
   }
 
-  /**
-   * Not empty.
-   *
-   * @param collection the collection
-   * @param message    the message
-   */
   public static void notEmpty(Collection<?> collection, String message) {
     if (collection == null || collection.size() == 0) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Not empty.
-   *
-   * @param array   the array
-   * @param message the message
-   */
   public static void notEmpty(Object[] array, String message) {
     if (array == null || array.length == 0) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Equals.
-   *
-   * @param a1      the a 1
-   * @param a2      the a 2
-   * @param message the message
-   */
   @SuppressWarnings("null")
-  public static void equals(Object a1, Object a2, String message) {
-    if (a1 == null && a2 == null) {
+  public static void equals(Object o1, Object o2, String message) {
+    if (o1 == null && o2 == null) {
       return;
-    } else if ((a1 == null && a2 != null) || (a1 != null && a2 == null)) {
+    } else if ((o1 == null && o2 != null) || (o1 != null && o2 == null)) {
       throw new IllegalArgumentException(message);
-    } else if (!a1.equals(a2)) {
+    } else if (!o1.equals(o2)) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Equals.
-   *
-   * @param <T> the type parameter
-   * @param a1  the a 1
-   * @param a2  the a 2
-   */
+  public static void equals(Object o1, Object o2) {
+    if (o1 == null && o2 == null) {
+      return;
+    } else if ((o1 == null && o2 != null) || (o1 != null && o2 == null)) {
+      throw new IllegalArgumentException("Expected " + o1 + ", but found " + o2);
+    } else if (!o1.equals(o2)) {
+      throw new IllegalArgumentException("Expected " + o1 + ", but found " + o2);
+    }
+  }
+
   @SuppressWarnings("null")
   public static <T> void equals(T[] a1, T[] a2) {
     if (a1 == null && a2 == null) {
@@ -160,12 +116,6 @@ public class Assert {
     }
   }
 
-  /**
-   * Equals.
-   *
-   * @param a1 the a 1
-   * @param a2 the a 2
-   */
   @SuppressWarnings("null")
   public static void equals(byte[] a1, byte[] a2) {
     if (a1 == null && a2 == null) {
@@ -181,12 +131,6 @@ public class Assert {
     }
   }
 
-  /**
-   * Length.
-   *
-   * @param string the string
-   * @param length the length
-   */
   public static void length(String string, int length) {
     if (string == null || string.length() != length) {
       throw new IllegalArgumentException("Unexpected string length: Expected string of length " + length + ", found: "
@@ -194,37 +138,18 @@ public class Assert {
     }
   }
 
-  /**
-   * Starts with.
-   *
-   * @param prefix the prefix
-   * @param string the string
-   */
   public static void startsWith(String prefix, String string) {
     if (string == null || !string.startsWith(prefix)) {
       throw new IllegalArgumentException("Expected prefix '" + prefix + "' is missing in: " + string);
     }
   }
 
-  /**
-   * Ends with.
-   *
-   * @param suffix the suffix
-   * @param string the string
-   */
   public static void endsWith(String suffix, String string) {
     if (string == null || !string.endsWith(suffix)) {
       throw new IllegalArgumentException("Expected suffix '" + suffix + "' is missing in: " + string);
     }
   }
 
-  /**
-   * Instance of.
-   *
-   * @param object the object
-   * @param type   the type
-   * @param name   the name
-   */
   public static void instanceOf(Object object, Class<?> type, String name) {
     if (object == null) {
       throw new IllegalArgumentException(name + " is not supposed to be null");
@@ -234,13 +159,6 @@ public class Assert {
     }
   }
 
-  /**
-   * Is true boolean.
-   *
-   * @param expression the expression
-   * @param message    the message
-   * @return the boolean
-   */
   public static boolean isTrue(boolean expression, String message) {
     if (!expression) {
       throw new IllegalArgumentException(message);
@@ -248,13 +166,6 @@ public class Assert {
     return expression;
   }
 
-  /**
-   * Is false boolean.
-   *
-   * @param expression the expression
-   * @param message    the message
-   * @return the boolean
-   */
   public static boolean isFalse(boolean expression, String message) {
     if (expression) {
       throw new IllegalArgumentException(message);
@@ -262,13 +173,6 @@ public class Assert {
     return expression;
   }
 
-  /**
-   * Found object.
-   *
-   * @param object the object
-   * @param name   the name
-   * @return the object
-   */
   public static Object found(Object object, String name) {
     if (object == null) {
       throw new IllegalArgumentException(name + " not found");
@@ -276,27 +180,12 @@ public class Assert {
     return object;
   }
 
-  /**
-   * Not negative.
-   *
-   * @param <E>   the type parameter
-   * @param value the value
-   * @param role  the role
-   */
   public static <E extends Number> void notNegative(E value, String role) {
     if (value.doubleValue() < 0) {
       throw new IllegalArgumentException(role + " is less than zero: " + value);
     }
   }
 
-  /**
-   * Positive e.
-   *
-   * @param <E>   the type parameter
-   * @param value the value
-   * @param role  the role
-   * @return the e
-   */
   public static <E extends Number> E positive(E value, String role) {
     if (value.doubleValue() <= 0) {
       throw new IllegalArgumentException(role + " is not positive: " + value);
@@ -304,14 +193,6 @@ public class Assert {
     return value;
   }
 
-  /**
-   * Negative e.
-   *
-   * @param <E>   the type parameter
-   * @param value the value
-   * @param role  the role
-   * @return the e
-   */
   public static <E extends Number> E negative(E value, String role) {
     if (value.doubleValue() >= 0) {
       throw new IllegalArgumentException(role + " is not negative: " + value);
@@ -319,53 +200,24 @@ public class Assert {
     return value;
   }
 
-  /**
-   * Less or equal.
-   *
-   * @param <T>       the type parameter
-   * @param value     the value
-   * @param threshold the threshold
-   * @param role      the role
-   */
   public static <T extends Comparable<T>> void lessOrEqual(T value, T threshold, String role) {
     if (value.compareTo(threshold) > 0) {
       throw new IllegalArgumentException(role + " is expected to be less than or equal to " + threshold + ", but is " + value);
     }
   }
 
-  /**
-   * That.
-   *
-   * @param flag    the flag
-   * @param message the message
-   */
   public static void that(boolean flag, String message) {
     if (!flag) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Not equals.
-   *
-   * @param d1      the d 1
-   * @param d2      the d 2
-   * @param message the message
-   */
   public static void notEquals(double d1, double d2, String message) {
     if (Math.abs(d1 - d2) < TOLERANCE) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Not zero e.
-   *
-   * @param <E>     the type parameter
-   * @param value   the value
-   * @param message the message
-   * @return the e
-   */
   public static <E extends Number> E notZero(E value, String message) {
     if (Math.abs(value.doubleValue()) < TOLERANCE) {
       throw new IllegalArgumentException(message);
@@ -373,24 +225,12 @@ public class Assert {
     return value;
   }
 
-  /**
-   * A number.
-   *
-   * @param triggerPrice the trigger price
-   * @param message      the message
-   */
   public static void aNumber(double triggerPrice, String message) {
     if (Double.isNaN(triggerPrice)) {
       throw new IllegalArgumentException(message);
     }
   }
 
-  /**
-   * Unique labels.
-   *
-   * @param array  the array
-   * @param errmsg the errmsg
-   */
   public static void uniqueLabels(Labeled[] array, String errmsg) {
     Set<String> labels = new HashSet<>();
     for (Labeled item : array) {
