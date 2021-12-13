4c4ecf120622643f3af5345f1e72f139e6872fa4
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-12-13 08:29:08 +0100

    New interface Validatable()

---
+++ src/main/java/com/rapiddweller/common/Validatable.java
@@ -0,0 +1,13 @@
+/* (c) Copyright 2021 by Volker Bergmann. All rights reserved. */
+
+package com.rapiddweller.common;
+
+/**
+ * Parent interface for all classes than can validate themselves.<br/><br/>
+ * Created: 12.12.2021 21:27:45
+ * @author Volker Bergmann
+ * @since 2.0.0
+ */
+public interface Validatable {
+  void validate();
+}
